cb8c669ceb197311a6e64c301bbae80bff67c0ea
Colin Detwiller <colinsr@me.com>
2016-04-12 06:25:57 -0400

    remove UseDefaultFiles() so that MVC will handle the routing

--- TheWorld_V2/src/TheWorld_V2/Startup.cs
+++ TheWorld_V2/src/TheWorld_V2/Startup.cs
@@ -16,7 +16,6 @@ namespace TheWorld_V2
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseDefaultFiles();
             app.UseStaticFiles();
         }
 
