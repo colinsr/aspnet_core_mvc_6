dc260ac692bc0bb70196bac92a95a44a53414397
Colin Detwiller <colinsr@me.com>
2016-04-12 06:39:03 -0400

    update controller to use MVC conventions

--- TheWorld_V2/src/TheWorld_V2/Controllers/Web/AppController.cs
+++ TheWorld_V2/src/TheWorld_V2/Controllers/Web/AppController.cs
@@ -1,7 +1,12 @@
+using Microsoft.AspNet.Mvc;
+
 namespace TheWorld_V2.Controllers.Web
 {
-    public class AppController
+    public class AppController : Controller
     {
-
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
