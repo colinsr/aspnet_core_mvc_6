ff2571fbd857fec7040747abd5c96ff8eab29bd3
Colin Detwiller <colinsr@me.com>
2016-04-12 08:09:18 -0400

    finish setting up Contact page, add ContactVM.

---
+++ TheWorld_V2/src/TheWorld_V2/ViewModels/ContactViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TheWorld_V2.ViewModels
+{
+    public class ContactViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; } 
+    }
+}
