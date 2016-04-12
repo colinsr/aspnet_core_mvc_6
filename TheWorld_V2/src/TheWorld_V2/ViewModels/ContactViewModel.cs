b7af8ec12ea70f9c90c97270e7aa36de63109fad
Colin Detwiller <colinsr@me.com>
2016-04-12 08:11:55 -0400

    add validation on ContactVM

--- TheWorld_V2/src/TheWorld_V2/ViewModels/ContactViewModel.cs
+++ TheWorld_V2/src/TheWorld_V2/ViewModels/ContactViewModel.cs
@@ -1,9 +1,17 @@
-﻿namespace TheWorld_V2.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheWorld_V2.ViewModels
 {
     public class ContactViewModel
     {
+        [Required]
+        [StringLength(255,MinimumLength = 5)]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [StringLength(1024, MinimumLength = 5)]
         public string Message { get; set; } 
     }
 }
