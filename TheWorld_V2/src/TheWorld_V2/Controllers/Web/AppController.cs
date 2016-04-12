9ce28f43472fbcf4820d286b2909c722127d34cc
Colin Detwiller <colinsr@me.com>
2016-04-12 10:23:22 -0400

    adding confirmation message on clientside, ensuring we have the email from the config, and clearing the form upon successful post.

--- TheWorld_V2/src/TheWorld_V2/Controllers/Web/AppController.cs
+++ TheWorld_V2/src/TheWorld_V2/Controllers/Web/AppController.cs
@@ -35,7 +35,15 @@ namespace TheWorld_V2.Controllers.Web
             {
                 var email = Startup.Configuration["AppSettings:SiteEmailAddress"];
 
-                _mailService.SendMail(email, email, $"Contact page from {model.Name} : {model.Email}", model.Message);
+                if (string.IsNullOrWhiteSpace(email))
+                    //empty string as first param translates to obj level error
+                    ModelState.AddModelError("", "Could not send email configuration problem.");
+
+                if (_mailService.SendMail(email, email, $"Contact page from {model.Name} : {model.Email}", model.Message))
+                {
+                    ModelState.Clear();//this will clear the form after post...
+                    ViewBag.Message = "Mail sent, thank you!";
+                }
             }
             
             return View();
