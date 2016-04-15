edc070d8d1e5b813d900cbb720c64c0e69c4cc1e
Colin Detwiller <colinsr@me.com>
2016-04-15 10:45:46 -0400

    adding new stops to DB and map

--- TheWorld_V2/src/TheWorld_V2/wwwroot/js/tripEditorController.js
+++ TheWorld_V2/src/TheWorld_V2/wwwroot/js/tripEditorController.js
@@ -13,26 +13,36 @@
         vm.isBusy = true;
         vm.newStop = {};
 
-        $http.get("/api/trips/" + vm.tripName + "/stops")
+        var url = "/api/trips/" + vm.tripName + "/stops";
+
+        $http.get(url)
             .then(function (response) {
                 debugger;
                 angular.copy(response.data, vm.stops);
                 _showMap(vm.stops);
-            }, function(error) {
-                vm.errorMessage = "Failed to load stops";
-            }).finally(function() {
+            }, function (error) { vm.errorMessage = "Failed to load stops"; })
+            .finally(function () {
                 vm.isBusy = false;
             });
+
+        vm.addStop = function() {
+            vm.isBusy = true;
+
+            $http.post(url, vm.newStop)
+                .then(function(response) {
+                    vm.stops.push(response.data);
+                    _showMap(vm.stops);
+                    vm.newStop = {};
+                }, function (error) { vm.errorMessage = "Failed to create new stop"; })
+                .finally(function () { vm.isBusy = false; });
+        }
     }
 
     function _showMap(stops) {
-
         if (stops && stops.length > 0) {
-
             var mapStops = _.map(stops, function (stop){ return { lat: stop.latitude, long: stop.longitude, info: stop.name } });
 
             travelMap.createMap({ stops: mapStops, selector: "#map", currentStop: 1, initialZoom: 3 });
         }
-
     }
 })();
