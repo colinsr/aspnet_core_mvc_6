031222bf18e979867957fd9cf7023182bb5c2c0c
Colin Detwiller <colinsr@me.com>
2016-04-15 07:25:59 -0400

    handle adding trips

--- TheWorld_V2/src/TheWorld_V2/wwwroot/js/tripsController.js
+++ TheWorld_V2/src/TheWorld_V2/wwwroot/js/tripsController.js
@@ -4,25 +4,40 @@
     angular.module("app-trips")
         .controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
 
         var vm = this;
 
-        vm.trips = [
-            {
-                name: "Us Trip",
-                created: Date.now()
-            },
-            {
-                name: "World Trip",
-                created: Date.now()
-            }
-        ];
+        vm.trips = [];
 
         vm.newTrip = {};
 
-        vm.addTrip = function() {
-            alert(vm.newTrip.name);
+        vm.errorMessage = "";
+        vm.isBusy = true;
+
+        $http.get("/api/trips")
+            .then(function (response) {
+                console.log(response.data);
+                angular.copy(response.data, vm.trips);
+            }, function(error) {
+                vm.errorMessage = "Failed to load data:  " + error;
+            }).finally(function() {
+                vm.isBusy = false;
+            });
+
+        vm.addTrip = function () {
+            vm.isBusy = true;
+            vm.errorMessage = "";
+
+            setTimeout(function() {}, 5000);
+
+            $http.post("/api/trips")
+                .then(function(response) {
+                    vm.trips.push(response.data);
+                    vm.newTrip = {};
+                }, function() {
+                    vm.errorMessage = "Failed to save new trip.";
+                }).finally();
         };
     };
 })();
