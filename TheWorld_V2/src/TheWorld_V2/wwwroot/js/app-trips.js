0419270f45c905a856d35fedd29efa25c33a35c1
Colin Detwiller <colinsr@me.com>
2016-04-15 08:36:19 -0400

    pulling out trip editor view

--- TheWorld_V2/src/TheWorld_V2/wwwroot/js/app-trips.js
+++ TheWorld_V2/src/TheWorld_V2/wwwroot/js/app-trips.js
@@ -6,6 +6,8 @@
         .config(function($routeProvider) {
             $routeProvider.when("/", { controller: "tripsController", controllerAs: "vm", templateUrl: "/views/tripsView.html" });
 
+            $routeProvider.when("/editor", { controller: "tripEditorController", controllerAs: "vm", templateUrl: "/views/tripEditorView.html" });
+
             $routeProvider.otherwise({ redirectTo: "/" });
         });
 
