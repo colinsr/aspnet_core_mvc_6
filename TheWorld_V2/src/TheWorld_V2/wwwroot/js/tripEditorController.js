a9382cf23c6bda98bf7b0c7f8d03d0fecc4c52b8
Colin Detwiller <colinsr@me.com>
2016-04-15 10:02:30 -0400

    showing stops on trip edit view

--- TheWorld_V2/src/TheWorld_V2/wwwroot/js/tripEditorController.js
+++ TheWorld_V2/src/TheWorld_V2/wwwroot/js/tripEditorController.js
@@ -4,12 +4,22 @@
     angular.module("app-trips")
         .controller("tripEditorController", tripEditorController);
 
-    function tripEditorController($routeParams) {
+    function tripEditorController($routeParams, $http) {
         var vm = this;
 
         vm.tripName = $routeParams.tripName;
-        vm.stencilOpSeparate = [];
+        vm.stops = [];
         vm.errorMessage = "";
         vm.isBusy = true;
+
+        $http.get("/api/trips/" + vm.tripName + "/stops")
+            .then(function (response) {
+                debugger;
+                angular.copy(response.data, vm.stops);
+            }, function(error) {
+                vm.errorMessage = "Failed to load stops";
+            }).finally(function() {
+                vm.isBusy = false;
+            });
     }
 })();
