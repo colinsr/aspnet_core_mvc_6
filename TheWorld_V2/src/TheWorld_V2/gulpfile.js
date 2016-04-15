ba5438491f788827db211cfc1ee8e167c8e7ee7d
Colin Detwiller <colinsr@me.com>
2016-04-15 11:59:30 -0400

    setting up prepublish/prepare scripts

--- TheWorld_V2/src/TheWorld_V2/gulpfile.js
+++ TheWorld_V2/src/TheWorld_V2/gulpfile.js
@@ -1,11 +1,13 @@
 ﻿/// <binding AfterBuild='minify' />
 "use strict";
 
-var gulp = require("gulp");
-var uglify = require("gulp-uglify");
+var gulp =          require("gulp");
+var uglify =        require("gulp-uglify");
+var ngAnnotate =    require("gulp-ng-annotate");
 
 gulp.task("minify", function () {
     return gulp.src("wwwroot/js/*.js")
+        .pipe(ngAnnotate())
         .pipe(uglify())
         .pipe(gulp.dest("wwwroot/lib/_app"));
 });
